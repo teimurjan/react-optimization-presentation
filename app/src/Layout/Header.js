5c3a7d4aa1099fde457f80bbf71cf52c91d4ccd0
Teimur Gasanov <teymurgg321@gmail.com>
2018-03-20 12:30:43 +0600

    Update navbar

--- app/src/Layout/Header.js
+++ app/src/Layout/Header.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NavItem from '../Common/NavItem';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+export default class extends React.Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired
+    };
 
-const Header = ({title}) => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">{title}</Link>
-        <div className="collapse navbar-collapse" id="navbarColor02">
-            <ul className="navbar-nav mr-auto">
-                <NavItem key={1} path='/'>Home</NavItem>
-                <NavItem key={2} path='/features'>Features</NavItem>
-                <NavItem key={3} path='/about'>About</NavItem>
-            </ul>
-        </div>
-    </nav>
-);
+    state = {
+        open: false
+    }
 
-Header.propTypes = {
-    title: PropTypes.string.isRequired
-}
+    handleTogglerClick = e => {
+        this.setState({ open: !this.state.open });
+    };
 
-export default Header;
+    render() {
+        const { title } = this.props;
+        return (
+            <nav className="navbar navbar-expand-xs navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">{title}</Link>
+                <button className="navbar-toggler" onClick={this.handleTogglerClick}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${this.state.open ? 'show' : ''}`} id="navbarColor02">
+                    <ul className="navbar-nav mr-auto">
+                        <NavItem key={1} path='/'>Home</NavItem>
+                        <NavItem key={2} path='/features'>Features</NavItem>
+                        <NavItem key={3} path='/about'>About</NavItem>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+};
