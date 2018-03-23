c5aa2b596d93586473d08824217f80f395cc8f6a
Teimur Gasanov <teymurgg321@gmail.com>
2018-03-23 14:32:58 +0600

    Change index as key scratch

--- scratches/index-as-key.js
+++ scratches/index-as-key.js
@@ -9,6 +9,6 @@ const elements = [
 const elements = [
     {type: 'div', key: 0, textContent: 'Container #0'},
     // Render будет бесполезно вызван для всех элементов ниже
-    {type: 'div', key: 2, textContent: 'Container #2'}, 
-    {type: 'div', key: 3, textContent: 'Container #3'},
+    {type: 'div', key: 1, textContent: 'Container #2'}, 
+    {type: 'div', key: 2, textContent: 'Container #3'},
 ];
